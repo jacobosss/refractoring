84c0ce0020167afe25a7172e918e78c59e2a2361
jacobosss <80431822+jacobosss@users.noreply.github.com>
2022-03-08 14:16:38 +0100

    ajout du mdp pour la connection a la bdd

--- src/mysql.connector.php
+++ src/mysql.connector.php
@@ -4,7 +4,7 @@ try
 {
     $db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=bddqcm',
         'root',
-        'yoo',
+        'root',
         array( PDO::ATTR_PERSISTENT => false, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
     );
 }
